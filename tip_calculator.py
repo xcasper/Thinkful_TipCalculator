b233e1f4ff1850fe65572b7736e8a7a856d05c9e
Craig Gleckman <cgleckman@gmail.com>
2014-01-11 22:49:54 -0700

    Updated User Input
    Added user input instead of constant values for the variables
    
    Thinkful TipCalculator exercise
    Version: 2

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,7 +1,7 @@
 def tip_calculator():
-    meal_cost = 20
-    tax_rate = .15
-    tip_rate = .15
+    meal_cost = int(raw_input("How much did the meal cost before tax? "))
+    tax_rate = int(raw_input("How much was the tax rate? ")) / 100.00
+    tip_rate = int(raw_input("How percentage would you like to tip? ")) / 100.00
     tax_amount = meal_cost * tax_rate
     tip_amount = (meal_cost + tax_amount) * tip_rate
     total = meal_cost + tax_amount + tip_amount
@@ -10,5 +10,6 @@ def tip_calculator():
     print "You need to pay $%.2f for tax" % tax_amount
     print "Tipping at a rate of %.2f%%, you should leave $%.2f for a tip." % ((tip_rate * 100), tip_amount)
     print "The grand total of your meal is $%.2f ." % total
+
 
 tip_calculator()
